ec129edf58b58d0a8aa62685d320e8878b8b3356
Steven Samy <stevensivo27@gmail.com>
2021-12-17 02:36:02 +0200

    url shortener

--- shortener/views.py
+++ shortener/views.py
@@ -6,10 +6,27 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'index.html')
 
-def create(request):
-    if request.method == ' POST ':
+
+
+def create (request):
+    if request.method == 'POST':
         link = request.POST['link']
-        uid =  str(uuid.uuid4())[:5]
-        new_url = Url(link=link, uuid=uid)
+        uid = str(uuid.uuid4())[:5]
+        new_url = Url(link=link,uuid=uid)
         new_url.save()
         return HttpResponse(uid)
+
+
+
+
+
+# def create (request):
+#     if request.method == ' POST ':
+#         link = request.POST['link']
+#         uid =  str(uuid.uuid4())[:5]
+#         new_url = Url(link=link, uuid=uid)
+#         new_url.save()
+#         return HttpResponse(uid)
+
+
+
