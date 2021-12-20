1e7ac2ce6f37ae478c304d6d30c08f6c62ca03d1
Steven Samy <stevensivo27@gmail.com>
2021-12-21 01:48:21 +0200

    Dinamic Url

--- shortener/urls.py
+++ shortener/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name ='index'),
-    path('create', views.create, name ='create')
+    path('create', views.create, name ='create'),
+    path('<str:pk>', views.go, name ='go')
+
 
 
 ]
